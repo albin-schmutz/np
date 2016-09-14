b76782675ff420042e3027eb2c14a5771f28762b
Albin <albin.schmutz@edorex.ch>
2016-09-14 14:16:32 +0200

    parser

--- compiler/obc_bootstrap/run.bat
+++ compiler/obc_bootstrap/run.bat
@@ -1,4 +1,6 @@
 copy ..\EnvLayer.np EnvLayer.m
 copy ..\NPC0.np NPC0.m
 obc EnvLayer.m NPC0.m -o npc0.exe
-npc0.exe < ..\NPC0.np
+call BatchSubstitute.bat "(*repl0*)" "(*" NPC0.m > tmp
+call BatchSubstitute.bat "(*repl1*)" "*)" tmp > NPC1.np
+npc0.exe < NPC1.np
