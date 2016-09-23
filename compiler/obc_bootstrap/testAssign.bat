2d1d44c92167e9e824ff9ce01a2ef03b2f4ea404
Albin <albin.schmutz@edorex.ch>
2016-09-23 13:25:33 +0200

    GenParam; simpler register allocation

---
+++ compiler/obc_bootstrap/testAssign.bat
@@ -0,0 +1,4 @@
+npc0.exe < ..\TestAssign.np
+del TestAssign.npx
+ren out TestAssign.npx
+..\..\runtime\npx.exe TestAssign.npx
---
+++ compiler/obc_bootstrap/testParam.bat
@@ -0,0 +1,4 @@
+npc0.exe < ..\TestParam.np
+del TestParam.npx
+ren out TestParam.npx
+..\..\runtime\npx.exe TestParam.npx
